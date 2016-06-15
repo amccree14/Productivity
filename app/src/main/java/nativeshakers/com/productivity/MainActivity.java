db9d79dbbcadf984eb44206663336cd18c48a87b
Kamoya Ikhofua <kki@mit.edu>
2016-06-14 22:56:45 -0700

    additional files

---
+++ app/src/main/java/nativeshakers/com/productivity/MainActivity.java
@@ -0,0 +1,35 @@
+package nativeshakers.com.productivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.firebase.client.Firebase;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView mappName;
+    TextView mwelcomeMessage;
+    Button mcreateAccount;
+    Button mlogin;
+    Button mdemo;
+    Firebase mRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mappName = (TextView)findViewById(R.id.appName);
+        mwelcomeMessage = (TextView)findViewById(R.id.welcomeMessage);
+        mcreateAccount = (Button)findViewById(R.id.createAccount);
+        mlogin = (Button)findViewById(R.id.login);
+        mdemo = (Button)findViewById(R.id.demo);
+        mRef = new Firebase("https://nativeshakers.com.productivity.firebaseio.com/appName");
+    }
+}
