6a177a6359a9ddc97197bf88a394ea7e27660d9b
Kamoya Ikhofua <kki@mit.edu>
2016-06-16 22:58:43 -0700

    User authentication completed.

--- app/src/main/java/nativeshakers/com/productivity/MainActivity.java
+++ app/src/main/java/nativeshakers/com/productivity/MainActivity.java
@@ -2,34 +2,154 @@ package nativeshakers.com.productivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.firebase.client.Firebase;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    TextView mappName;
-    TextView mwelcomeMessage;
-    Button mcreateAccount;
-    Button mlogin;
-    Button mdemo;
-    Firebase mRef;
+public class MainActivity extends AppCompatActivity {
+    private List<Task> taskList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private TaskAdapter tAdapter;
+    Firebase myFirebaseRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
+        //Creates a reference for the FIREBASE database
+        myFirebaseRef = new Firebase("https://nsproductivity.firebaseio.com");
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        tAdapter = new TaskAdapter(taskList);
+
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(tAdapter);
+
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Task task = taskList.get(position);
+                Toast.makeText(getApplicationContext(), task.getTitle() + " is finished!", Toast.LENGTH_SHORT).show();
+                taskList.remove(task);
+                tAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+            }
+
+        }));
+
+        dummyTaskData();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        mappName = (TextView)findViewById(R.id.appName);
-        mwelcomeMessage = (TextView)findViewById(R.id.welcomeMessage);
-        mcreateAccount = (Button)findViewById(R.id.createAccount);
-        mlogin = (Button)findViewById(R.id.login);
-        mdemo = (Button)findViewById(R.id.demo);
-        mRef = new Firebase("https://nsproductivity.firebaseio.com/appName");
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_logout:
+                myFirebaseRef.unauth();
+                finish();
+                return true;
+            case R.id.action_plus:
+                Intent i = new Intent(MainActivity.this, TaskDetails.class);
+                startActivity(i);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
+
+    private void dummyTaskData() {
+        Task task = new Task("Finish Vector Calc HW", "Homework", "7:00 P.M.");
+        taskList.add(task);
+
+        task = new Task("Clean up dog shit", "Housework", "5:00 P.M.");
+        taskList.add(task);
+
+        task = new Task("Clean up dog shit", "Housework", "5:00 P.M.");
+        taskList.add(task);
+
+        tAdapter.notifyDataSetChanged();
+    }
+
+    public interface ClickListener {
+        void onClick(View view, int position);
+
+        void onLongClick(View view, int position);
+    }
+
+    public static class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
+
+        private GestureDetector gestureDetector;
+        private MainActivity.ClickListener clickListener;
+
+        public RecyclerTouchListener(Context context, final RecyclerView recyclerView, final MainActivity.ClickListener clickListener) {
+            this.clickListener = clickListener;
+            gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
+                }
+
+                @Override
+                public void onLongPress(MotionEvent e) {
+                    View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+                    if (child != null && clickListener != null) {
+                        clickListener.onLongClick(child, recyclerView.getChildPosition(child));
+                    }
+                }
+            });
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+
+            View child = rv.findChildViewUnder(e.getX(), e.getY());
+            if (child != null && clickListener != null && gestureDetector.onTouchEvent(e)) {
+                clickListener.onClick(child, rv.getChildPosition(child));
+            }
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+        }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+        }
+    }
+
+
 }
--- app/src/main/java/nativeshakers/com/productivity/Task.java
+++ app/src/main/java/nativeshakers/com/productivity/Task.java
@@ -6,36 +6,41 @@ import java.util.HashMap;
  * Created by earlybirdcamp on 6/14/16.
  */
 public class Task {
-    String text;
-    String category;
-
-    public enum Category {
-        SCHOOLWORK, HOUSEWORK, PERSONAL, OTHER
-    }
-    // Task t = new Task("Wash dishes",Task.CATEGORY);
+    private String title, category, time;
 
     public Task() {
-        this.text = "Not Set";
-        this.category = "Not Set";
     }
 
-    public Task(String text, String category){
-        this.text = text;
+    public Task(String title, String category, String time) {
+        this.title = title;
         this.category = category;
+        this.time= time;
     }
 
-    public int getScore(){
-        switch(this.category){
-            case "SCHOOLWORK":
-                return 10;
-            case "HOUSEWORK":
-                return 20;
-            case "PERSONAL":
-                return 30;
-            case "OTHER":
-                return 40;
-            default:
-                return 0;
-        }
+
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String name) {
+        this.title = name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
     }
 }
